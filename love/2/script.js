de95c3b8e46ee61a59ddc387d0560c30a5b1e5c2
pdchinh <pdc1761.n@gmail.com>
2021-12-16 23:17:44 +0700

    noel

---
+++ love/2/script.js
@@ -0,0 +1,47 @@
+snowDrop(150, randomInt(1035, 1280));
+snow(150, 150);
+$(document).ready(function() {
+    // process bar
+
+    var audio0 = new Audio('sound/sound.mp3');
+    audio0.play();
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function snow(num, speed) {
+		if (num > 0) {
+			setTimeout(function () {
+				$('#drop_' + randomInt(1, 250)).addClass('animate');
+				num--;
+				snow(num, speed);
+			}, speed);
+		}
+	};
+
+	function snowDrop(num, position) {
+		if (num > 0) {
+			var drop = '<div class="drop snow" id="drop_' + num + '"></div>';
+
+			$('body').append(drop);
+			$('#drop_' + num).css('left', position);
+			num--;
+			snowDrop(num, randomInt(60, 1280));
+		}
+	};
+
+function randomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	};
+
+function playAudio(){
+
+    var audio0 = new Audio('sound/sound.mp3');
+    audio0.play();
+};
---
+++ love/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Cậu yêu tớ có phải không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'cậu mơ à :333',
+    question:'Yêu quá!!! Nói cho tớ lý do vì sao cậu yêu tớ đii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì cậu đẹp trai <33333333',
+    mess: 'Tớ biết mà 🥰. Love you 😘😘',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/kr3w176' 
+}
