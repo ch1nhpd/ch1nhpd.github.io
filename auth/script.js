7cad3085ae85eb72fd4fdcb5339f4434e6e73c40
pdchinh <pdc1761.n@gmail.com>
2022-08-26 23:44:42 +0700

    rebuilding site Fri, Aug 26, 2022 23:44:41

---
+++ auth/script.js
@@ -0,0 +1,11 @@
+var btnLogin = document.getElementById('do-login');
+var idLogin = document.getElementById('login');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+btnLogin.onclick = function(){
+  // vào đến đây rồi thì xem đi :)
+  if (username.value=="Quỳnh" && password.value=="<3") { 
+     location="https://ch1nhpd.github.io/love-QccNt09/" ;
+    } 
+    else { alert("Đã bảo không cho xem mà!") } 
+}
