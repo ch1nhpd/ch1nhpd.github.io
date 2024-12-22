044f0a8fb7894d875ea37aff1ad252b82aa372c6
pdchinh <pdc1761.n@gmail.com>
2024-12-22 15:20:23 +0700

    rebuilding site Sun, Dec 22, 2024 15:20:23

--- auth/script.js
+++ auth/script.js
@@ -5,12 +5,12 @@ btnLogin.onclick = function(){
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 const pass = username + password + username;
-const h1 = CryptoJS.SHA512(pass).toString();
-var path = h1.substring(h1.length / 2 - 8, h1.length / 2 + 8) + pass.substring(pass.length / 2 - 5, pass.length / 2 + 5);
+const h1 = CryptoJS.SHA512(pass).toString(); 
+var path = h1.substring(h1.length / 2 - 5, h1.length / 2 + 5);// + pass.substring(pass.length / 2 - 5, pass.length / 2 + 5);
 
 const h2 = CryptoJS.SHA512(h1+path+pass).toString();
   // vào đến đây rồi thì xem đi :)
-  if (h2=="d22fb96c1cfd877301aa696074e16c20bac950982c77c3c11766dc5c9c687b3e3cdc6f1a6be84adb2552e14d62a249019ebf53823110ef7d5bcad927c67ffc12") { 
+  if (h2=="080b97e4af032faf262d218dc09016a46c3c90442dce37d1efd72fa18241df0f320b730bbd0b88e599151ff01bdb7edcdb1235c4c751b415ba849135dde4d446") { 
      location="https://ch1nhpd.github.io/"+path ;
     } 
     else { alert("Đã bảo không cho xem mà!"+h2) } 
